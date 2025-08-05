147e5097df4617b3dacbbb5e015a8a0ff202d9e0
lola212121 <nikaloladze06@gmail.com>
2025-08-05 19:00:08 +0400

    upadted style js and html
    alright so i added buttons in alert div
    also i add so when i slick on save btn and save note div background colors will be randomly chosen from only four colors also i add clear all note btn and when i click it it gives me second chanse that says do you want to delete this and it has two btn yes and no   both of  them work

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const textPart = document.getElementById("writing-area")
 const clearBtn = document.querySelector(".clear-btn") 
 const headline = document.querySelector(".notes-headline-writing-tag")
 const body = document.body
+const clearAll = document.querySelector(".history-clear-btn")
 
 
 
@@ -26,9 +27,18 @@ saveBtn.addEventListener('click', () => {
         alertDiv.appendChild(alertText)
 
         // alert button for closing div
+        const alertButton = document.createElement("button")
+        alertButton.classList.add("alert-button")
+        alertButton.innerHTML = 'close'
+
+        alertButton.addEventListener('click' , () => {
+            alertDiv.classList.add("hide")
+        })
 
 
 
+        alertDiv.appendChild(alertButton)
+        
 
 
 
@@ -69,6 +79,17 @@ saveBtn.addEventListener('click', () => {
 
 
 
+    
+    // this is for generating random collors 
+    function changeColor(){
+        const colors = ["#ff4545" , "#00ff9988" , "#006aff" , "#ff0095"]
+        const randomColor = Math.floor(Math.random() * colors.length)
+        noteDiv.style.backgroundColor = colors[randomColor]
+    }
+
+    changeColor()
+
+
     // this is for after  you click text and headline to desapear
         textPart.value = ''
         headline.value = ''
@@ -82,3 +103,51 @@ saveBtn.addEventListener('click', () => {
 clearBtn.addEventListener("click" , () => {
     textPart.value = '';
 })
+
+// this is for clearing all notes by one click on btn
+clearAll.addEventListener("click", () => {
+    // this is so when we click to create new div 
+    const delDiv = document.createElement("div")
+    delDiv.classList.add("del-div")
+    body.appendChild(delDiv)
+
+
+    // this is text in div
+    const divText = document.createElement("p")
+    divText.innerHTML = "Are you sure you want to delete all notes. This action cannot be undone."
+    divText.classList.add("del-text")
+    delDiv.appendChild(divText)
+
+
+    // creating yes and no button 
+    const yesBtn = document.createElement("button")
+    const noBtn = document.createElement("button")
+
+
+    yesBtn.classList.add("yes-button")
+    yesBtn.innerHTML = 'yes'
+    noBtn.classList.add("no-button")
+    noBtn.innerHTML = 'no'
+
+
+    yesBtn.addEventListener('click', () => {
+        noteContainer.innerHTML = ''
+        delDiv.classList.add("hide")
+    })
+
+    noBtn.addEventListener('click', () => {
+        delDiv.classList.add("hide")    
+    })
+
+
+    delDiv.appendChild(yesBtn)
+
+
+    delDiv.appendChild(noBtn)
+
+
+})
+
+
+
+
